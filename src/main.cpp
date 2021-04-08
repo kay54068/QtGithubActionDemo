3960e9707ecd24d116580140d3a26a006233855a
max.chen <max540688@gmail.com>
2021-04-08 23:38:43 +0800

    feat: add Qt Demo Project

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+#include <QtCore/qglobal.h>
+#if QT_VERSION >= 0x050000
+#include <QtGui/QGuiApplication>
+#include <QtQml/QQmlApplicationEngine>
+#else
+#endif
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+    const QUrl url(QStringLiteral("qrc:/main.qml"));
+    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
+                     &app, [url](QObject *obj, const QUrl &objUrl) {
+        if (!obj && url == objUrl)
+            QCoreApplication::exit(-1);
+    }, Qt::QueuedConnection);
+    engine.load(url);
+
+    return app.exec();
+}
---
+++ src/main.qml
@@ -0,0 +1,14 @@
+import QtQuick 2.0
+import QtQuick.Window 2.0
+
+Window {
+    width: 640
+    height: 480
+    visible: true
+    title: qsTr("Qt Github Action Demo")
+    Text {
+        text: "Run Demo on " + Qt.platform.os
+        font.pixelSize: 30
+        anchors.centerIn: parent
+    }
+}
